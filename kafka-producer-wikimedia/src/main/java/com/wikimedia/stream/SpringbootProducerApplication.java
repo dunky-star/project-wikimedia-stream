d0e5b78126b5829dcb5909107340c0983e270387
dunky-star <dunkygeoffrey39@gmail.com>
2024-01-06 20:24:20 -0600

    Add Wikimedia Event Handler

--- kafka-producer-wikimedia/src/main/java/com/wikimedia/stream/SpringbootProducerApplication.java
+++ kafka-producer-wikimedia/src/main/java/com/wikimedia/stream/SpringbootProducerApplication.java
@@ -1,11 +1,22 @@
 package com.wikimedia.stream;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootProducerApplication {
+public class SpringbootProducerApplication implements CommandLineRunner {
+    public SpringbootProducerApplication(WikimediaChangesProducer wikimediaChangesProducer) {
+        this.wikimediaChangesProducer = wikimediaChangesProducer;
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(SpringbootProducerApplication.class);
+    }
+
+    private final WikimediaChangesProducer wikimediaChangesProducer;
+    @Override
+    public void run(String... args) throws Exception {
+        wikimediaChangesProducer.sendMessage();
     }
 }
