ee80f51e45bd1335cace6dff838645d759caae78
dunky-star <dunkygeoffrey39@gmail.com>
2024-01-06 19:49:17 -0600

    Add dependency for Wikimedia Events

--- kafka-producer-wikimedia/src/main/java/com/wikimedia/stream/WikimediaChangesProducer.java
+++ kafka-producer-wikimedia/src/main/java/com/wikimedia/stream/WikimediaChangesProducer.java
@@ -1,7 +1,19 @@
 package com.wikimedia.stream;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 public class WikimediaChangesProducer {
+    private static final Logger LOGGER = LoggerFactory.getLogger(WikimediaChangesProducer.class);
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    public WikimediaChangesProducer(KafkaTemplate<String, String> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+
 }
