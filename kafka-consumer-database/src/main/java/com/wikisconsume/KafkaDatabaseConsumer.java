81bc42f2fca9a329676414705462ef41448c9a1c
dunky-star <dunkygeoffrey39@gmail.com>
2024-01-08 10:54:08 -0600

    Add Wikimedia event consumer -  database

--- kafka-consumer-database/src/main/java/com/wikisconsume/KafkaDatabaseConsumer.java
+++ kafka-consumer-database/src/main/java/com/wikisconsume/KafkaDatabaseConsumer.java
@@ -3,7 +3,9 @@ package com.wikisconsume;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
 
+@Service
 public class KafkaDatabaseConsumer {
     private static final Logger LOGGER = LoggerFactory.getLogger(KafkaDatabaseConsumer.class);
 
